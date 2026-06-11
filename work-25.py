744797610582551db5a9863455e43194985c1b27
rrozmetov992 <rrozmetov992@gmail.com>
2026-06-11 08:29:28 +0300

    Add files via upload

---
+++ work-25.py
@@ -0,0 +1,62 @@
+#16-misol
+#a=float(input("a="))
+#n=int(input("n="))
+#s=1
+#for i in range(n):
+#    s*=a**i
+#    print(s)  
+
+#17-misol
+#a=float(input("a="))
+#n=int(input("n="))
+#s=0
+#for i in range(0, n+1):
+#    s+=a**i
+#    print(s) 
+
+#18-misol
+#a=float(input("a="))
+#n=int(input("n="))
+#s=0
+#for i in range(0, n+1):
+#    s+=(-1)**(i)*a**i
+#    print(s) 
+
+#19-misol
+#n=int(input("n="))
+#s=1
+#for i in range(1, n+1):
+#    s*=i
+#print(s)
+
+#20-misol
+#n=int(input("n="))
+#c=1
+#s=0
+#for i in range(1, n+1):
+#    c*=i
+#   s+=c
+#    print(s)
+#21-misol
+#n=int(input("n="))
+#c=1.0  
+#f=1
+#for i in range(1, n+1):
+ #   f*=i
+  #  c+=1/f
+   # print("natija:", c)
+   
+
+
+
+#22-misol
+ #   n = int(input("n = "))
+  #  x = float(input("x = "))
+  #  S = 1
+  #  factorial = 1
+#    for i in range(1, n + 1):
+#        factorial *= i
+#        S += x**i / factorial
+#    print("Yig'indi =", S)
+#    print("math.exp(x) =", math.exp(x))
+
